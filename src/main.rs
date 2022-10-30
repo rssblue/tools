e31adfdd0c5052c4e296ea1e0a42d7c1a8db9153
Dovydas <dovydas@yoshke.org>
2022-10-30 19:10:00 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,130 @@
+use sycamore::prelude::*;
+use sycamore_router::{HistoryIntegration, Route, Router};
+use uuid::Uuid;
+
+#[derive(Route)]
+enum AppRoutes {
+    #[to("/")]
+    Index,
+    #[to("/podcast-guid")]
+    PodcastGuid,
+    #[not_found]
+    NotFound,
+}
+
+fn main() {
+    sycamore::render(|cx| {
+        view! { cx,
+            Router(
+                integration=HistoryIntegration::new(),
+                view=|cx, route: &ReadSignal<AppRoutes>| {
+                    view! { cx,
+                        Common {
+                            (match route.get().as_ref() {
+                                AppRoutes::Index => view!{ cx, Index{}},
+                                AppRoutes::PodcastGuid => view!{ cx, PodcastGuid{}},
+                                AppRoutes::NotFound => view! { cx,
+                                    "404 Not Found"
+                                },
+                            })
+                        }
+                    }
+                }
+            )
+        }
+    });
+}
+
+#[derive(Prop)]
+pub struct MyComponentProps<'a, G: Html> {
+    children: Children<'a, G>,
+}
+
+#[component]
+fn Common<'a, G: Html>(cx: Scope<'a>, props: MyComponentProps<'a, G>) -> View<G> {
+    let children = props.children.call(cx);
+    view! { cx,
+    a(href="https://rssblue.com") {
+        img(
+            src="https://rssblue.com/static/dist/img/logo.svg",
+            class="h-10 mx-auto mt-3 mb-7",
+            alt="RSS Blue Logo",
+            ){}
+    }
+    main(class="flex-grow") {
+        div(class="pt-4 pb-7 mx-auto px-2 lg:px-0 max-w-prose") {
+        (children)
+        }
+    }
+    }
+}
+
+#[component]
+fn Index<G: Html>(cx: Scope) -> View<G> {
+    view! { cx,
+    h1 { "Tools" }
+    div(class="grid grid-cols-1 md:grid-cols-2 gap-2") {
+        a(
+            class=format!("btn btn-primary"),
+            href="/podcast-guid",
+            ) { "Podcast GUID" }
+    }
+    }
+}
+
+#[component]
+fn PodcastGuid<G: Html>(cx: Scope) -> View<G> {
+    const NAMESPACE_PODCAST: uuid::Uuid = uuid::Uuid::from_bytes([
+        0xea, 0xd4, 0xc2, 0x36, 0xbf, 0x58, 0x58, 0xc6, 0xa2, 0xc6, 0xa6, 0xb2, 0x8d, 0x12, 0x8c,
+        0xb6,
+    ]);
+
+    let url = create_signal(cx, String::new());
+    let guid = create_signal(cx, String::new());
+    create_effect(cx, || {
+        let uuid = Uuid::new_v5(&NAMESPACE_PODCAST, url.get().as_bytes());
+        guid.set(uuid.to_string());
+    });
+
+    view! { cx,
+    h1 { "Podcast GUID" }
+    h2(class="text-gray-500") { "Generate a unique, global identifier for your podcast." }
+    form(class="space-y-4") {
+        // Prevent submission with "Enter".
+        button(
+            type="submit",
+            disabled=true,
+            style="display: none",
+            aria-hidden="true"
+            ){}
+        div{
+            label(for="url") { "Podcast feed's URL" }
+            input(
+                class="input-text",
+                type="url",
+                id="url",
+                placeholder="example.com/podcast-feed",
+                autocomplete="off",
+                bind:value=url,
+                )
+        }
+        (if !url.get().to_string().trim().is_empty() {
+            view! { cx,
+            div {
+                "GUID"
+                div(
+                    type="text",
+                    class="input-text select-all",
+                    ) {
+                    (guid.get())
+                }
+            }
+            }
+        } else {
+            view! { cx, }
+        })
+
+    }
+
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,79 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./index.html", "./src/main.rs"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins"],
+      mono: ["Source Code Pro"],
+    },
+    extend: {
+      colors: {
+        blue: {
+          50: "#E0ECF9",
+          100: "#B2D1EF",
+          200: "#87B7E6",
+          300: "#5F9EDC",
+          400: "#3A87D3",
+          500: "#1871C9",
+          600: "#0F5DA9",
+          700: "#08498A",
+          800: "#03376A",
+          900: "#00254A",
+        },
+        red: {
+          50: "#F9E3E5",
+          100: "#F3C6CA",
+          200: "#EDAAB0",
+          300: "#E78D96",
+          400: "#E1717B",
+          500: "#DB5461",
+          600: "#C82A39",
+          700: "#96202B",
+          800: "#64151D",
+          900: "#320B0E",
+        },
+        yellow: {
+          50: "#FFF4D4",
+          100: "#FEECB8",
+          200: "#FDE59D",
+          300: "#FCDD81",
+          400: "#FBD566",
+          500: "#FBCD4B",
+          600: "#D9AE31",
+          700: "#B78F1B",
+          800: "#95720B",
+          900: "#735600",
+        },
+        green: colors.teal,
+        primary: colors.blue,
+        secondary: colors.yellow,
+        warning: colors.yellow,
+        danger: colors.red,
+        success: colors.green,
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            strong: "none",
+            img: "none",
+            figure: "none",
+            a: "none",
+            code: "none",
+            "code::before": {
+              content: "none",
+            },
+            "code::after": {
+              content: "none",
+            },
+            pre: "none",
+            "pre code": {
+              "white-space": "pre-wrap",
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+};
