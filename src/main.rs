34b2cdf9b3c9c1973dcca4e333a40036d7df949a
Dovydas <dovydas@yoshke.org>
2022-10-31 20:13:26 +0000

    update title

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
-use rocket::fs::NamedFile;
 use rocket::fs::{relative, FileServer};
 use rocket::get;
 use rocket::launch;
-use std::path::Path;
+use rocket::response::content::RawHtml;
+
+static INDEX_FILE: &str = include_str!("../app/dist/index.html");
 
 #[get("/")]
-async fn index() -> Option<NamedFile> {
-    common().await
+async fn index() -> RawHtml<String> {
+    RawHtml(INDEX_FILE.replace("<title></title>", "<title>RSS Blue Tools</title>"))
 }
 
 #[get("/podcast-guid")]
-async fn podcast_guid() -> Option<NamedFile> {
-    common().await
-}
-
-async fn common() -> Option<NamedFile> {
-    let path = Path::new(relative!("/app/dist-html/index.html"));
-
-    NamedFile::open(path).await.ok()
+async fn podcast_guid() -> RawHtml<String> {
+    RawHtml(INDEX_FILE.replace(
+        "<title></title>",
+        "<title>Podcast GUID | RSS Blue Tools</title>",
+    ))
 }
 
 #[launch]
