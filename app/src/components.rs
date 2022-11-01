59f98291de6a315aaf52c39eb588df1a8c5bd38f
Dovydas <dovydas@yoshke.org>
2022-11-01 10:11:37 +0000

    improve icon display

--- app/src/components.rs
+++ app/src/components.rs
@@ -31,6 +31,19 @@ struct Warning {
     msg: String,
 }
 
+enum Icon {
+    AlertCircle(String),
+}
+
+impl std::fmt::Display for Icon {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            Self::AlertCircle(classes) => format!("<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' class='{classes}'><circle cx='12' cy='12' r='10'></circle><line x1='12' y1='8' x2='12' y2='12'></line><line x1='12' y1='16' x2='12.01' y2='16'></line></svg>"),
+        };
+        write!(f, "{s}")
+    }
+}
+
 #[derive(Prop)]
 struct WarningComponentProps {
     warning: Warning,
@@ -44,8 +57,7 @@ fn WarningComponent<G: Html>(cx: Scope, props: WarningComponentProps) -> View<G>
         role="alert",
         ) {
         span(
-            class="inline flex-shrink-0 mr-3 w-6 h-6 stroke-2",
-            dangerously_set_inner_html="<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' class='feather feather-alert-circle'><circle cx='12' cy='12' r='10'></circle><line x1='12' y1='8' x2='12' y2='12'></line><line x1='12' y1='16' x2='12.01' y2='16'></line></svg>",
+            dangerously_set_inner_html=Icon::AlertCircle("inline flex-shrink-0 mr-3 w-6 h-6 stroke-2".to_string()).to_string().as_str(),
             ){}
         span(dangerously_set_inner_html=props.warning.msg.as_str()){}
     }
