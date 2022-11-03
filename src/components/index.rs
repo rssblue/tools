be4d53a776dc41ca8accee16b75767bb480eaeb4
Dovydas <dovydas@yoshke.org>
2022-11-03 10:48:45 +0000

    refactor components

---
+++ src/components/index.rs
@@ -0,0 +1,14 @@
+use sycamore::prelude::*;
+
+#[component]
+pub fn Index<G: Html>(cx: Scope) -> View<G> {
+    view! { cx,
+    h1 { "Tools" }
+    div(class="grid grid-cols-1 md:grid-cols-2 gap-2") {
+        a(
+            class=format!("btn btn-primary"),
+            href="/podcast-guid",
+            ) { "Podcast GUID" }
+    }
+    }
+}
